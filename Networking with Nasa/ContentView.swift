6b8065fe686a4eb70c8141236ebed9a3de0b5fb3
kylekaufman <ktk33@case.edu>
2025-02-20 11:06:07 -0500

    Initial Commit

---
+++ Networking with Nasa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Networking with Nasa
+//
+//  Created by Kyle Kaufman on 2/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Networking with Nasa/Networking_with_NasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_with_NasaApp.swift
+//  Networking with Nasa
+//
+//  Created by Kyle Kaufman on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Networking_with_NasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
